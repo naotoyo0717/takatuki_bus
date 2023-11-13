51cdcf982a4252ae45ea7e17bd9116a077752094
sDaigo <s8778@g.maizuru-ct.ac.jp>
2023-11-13 15:16:08 +0900

    dbをjsonからsqlite3に変更

--- yoyaku_system/bottle_yoyaku.py
+++ yoyaku_system/bottle_yoyaku.py
@@ -1,32 +1,42 @@
 from bottle import Bottle, route, run, template, request
-import json
+import sqlite3
 
-yoyaku_seki = []
+# SQLite3データベースに接続
+conn = sqlite3.connect('yoyaku_seki.db')
+cursor = conn.cursor()
 
-try:
-    with open('yoyaku_seki.json','r') as file:  
-        yoyaku_seki = json.load(file)  # 予約済みの席をデータベースから取得
-except FileNotFoundError:
-    yoyaku_seki = []
+# テーブルが存在しない場合は作成
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS yoyaku_seki (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        seat_number INTEGER
+    )
+''')
+conn.commit()
 
 app = Bottle()
 
 @app.route('/')
 def index():
+    cursor.execute('SELECT seat_number FROM yoyaku_seki')
+    yoyaku_seki = [row[0] for row in cursor.fetchall()]
     return template('index_template', yoyaku_seki=yoyaku_seki)
 
 @app.route('/yoyaku', method='POST')
 def yoyaku():
     seat_number = int(request.forms.get('seat_number'))
-    # こちらで任意の処理を行う
-    if seat_number in yoyaku_seki:
+
+    # 席がすでに予約されているか確認
+    cursor.execute('SELECT seat_number FROM yoyaku_seki WHERE seat_number = ?', (seat_number,))
+    existing_reservation = cursor.fetchone()
+
+    if existing_reservation:
         print(f"席{seat_number}の予約が失敗しました")
         return "予約失敗"
     else:
-        yoyaku_seki.append(seat_number)
-        # データベースに保存
-        with open('yoyaku_seki.json', 'w',encoding='utf-8') as file:
-            json.dump(yoyaku_seki, file, ensure_ascii=False)
+        # 席を予約する
+        cursor.execute('INSERT INTO yoyaku_seki (seat_number) VALUES (?)', (seat_number,))
+        conn.commit()
         print(f"席{seat_number}が予約されました！")
         return "予約完了"
 
